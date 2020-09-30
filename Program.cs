28251ac37b4132cb4c1b66e180054fe1131ddf6e
manasachervirala <72153500+manasachervirala@users.noreply.github.com>
2020-09-30 21:52:18 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace naturalnumbers
+{
+    class naturalnumber
+    {
+        static void Main(string[] args)
+        {
+         
+
+                int i;
+              
+                Console.WriteLine("The first 10 natural number are:");
+
+                for (i = 1; i <= 10; i++)
+                {
+                    Console.Write("{0} ", i);
+                Console.ReadLine();
+                }
+                
+            }
+        }
+    }
+    
+
