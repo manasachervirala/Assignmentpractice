6e11188a6ea07bd207eb58e506b4a60cf363a794
manasachervirala <72153500+manasachervirala@users.noreply.github.com>
2020-10-01 20:18:20 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,29 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace amstrong
+namespace array_accending
 {
-   
-  public class ArmstrongExample
+    class arrayaccending
+    {
+        static void Main(string[] args)
         {
-            public static void Main(string[] args)
-            {
-                int n, r, sum = 0, temp;
-                Console.Write("Enter the Number= ");
-                n = int.Parse(Console.ReadLine());
-                temp = n;
-                while (n > 0)
+
+                int i;
+                int[] a = new int[30]; 
+                Console.Write("Enter the Number of values to be Sort : ");
+                
+                int n = Convert.ToInt16(Console.ReadLine());
+           
+                for (i = 1; i <= n; i++)
                 {
-                    r = n % 10;
-                    sum = sum + (r * r * r);
-                    n = n / 10;
+                    Console.Write("Enter the No " + i + ":");
+                      
+                    a[i] = Convert.ToInt16(Console.ReadLine());
                 }
-                if (temp == sum)
-                    Console.Write("Armstrong Number.");
-                else
-                    Console.Write("Not Armstrong Number.");
-            Console.ReadLine();
+                
+                for (i = 1; i <= n; i++)
+                {
+                    for (int j = 1; j <= n - 1; j++)
+                    {
+                        if (a[j] > a[j + 1])
+                        {
+                            int temp = a[j];
+                            a[j] = a[j + 1];
+                            a[j + 1] = temp;
+                        }
+                    }
+                }
+               
+                Console.Write("Ascending Sort : ");
+                for (i = 1; i <= n; i++)
+                {
+                    Console.Write(a[i] + " ");
+                }
+                
+                Console.ReadKey();
             }
         }
     }
+
 
