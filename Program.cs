8eac21dd7e183e9edda7c295e48451c03cd45cf5
manasachervirala <72153500+manasachervirala@users.noreply.github.com>
2020-10-01 20:22:02 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,57 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FirstNprimenumbers
+namespace largestnumberinarray
 {
-    class Program
+    class largestnumberinarray
     {
-  
 
-            // Function to print first N prime numbers  
-            static void print_first_N_primes(int N)
+            static int[] arr = { 89, 3243, 4590, 90234, 93808 };
+
+            
+            static int largest()
             {
-                // Declare the variables  
-                int i, j, flag;
+                int i;
+           int max = arr[0];
 
-                // Print display message  
-                Console.Write("Prime numbers between 1 and " +
-                                               N + " are:\n");
+              
+                for (i = 1; i < arr.Length; i++)
+                    if (arr[i] > max)
+                        max = arr[i];
 
-                // Traverse each number from 1 to N  
-                // with the help of for loop  
-                for (i = 1; i <= N; i++)
-                {
-
-                    // Skip 0 and 1 as they are  
-                    // niether prime nor composite  
-                    if (i == 1 || i == 0)
-                        continue;
-
-                     
-                    flag = 1;
-
-                    for (j = 2; j <= i / 2; ++j)
-                    {
-                        if (i % j == 0)
-                        {
-                            flag = 0;
-                            break;
-                        }
-                    }
-                    
-                    if (flag == 1)
-                        Console.Write(i + " ");
-                Console.ReadLine();
-                }
+                return max;
             }
 
-         
-            public static void Main(String[] args)
+             
+            public static void Main()
             {
-                int N = 100;
-
-                print_first_N_primes(N);
+                Console.WriteLine("Largest in given " + largest());
+            Console.ReadLine();
             }
         }
+
     }
 
