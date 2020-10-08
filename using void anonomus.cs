a6d057a124a789b526ae22037a17443aeec784b2
manasachervirala <72153500+manasachervirala@users.noreply.github.com>
2020-10-08 13:32:52 +0530

    Add files via upload

---
+++ using void anonomus.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace voiddelegate
+{
+    public delegate void AddSumDelegate(int a, int b);
+    public delegate void MulDelegate(int a);
+    class Program
+    {
+        static int num = 10;
+        public static void AddSum(int a,int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        public static void Mul(int a)
+        {
+            num *= a;
+            Console.WriteLine("given method is: {0}",num );
+        }
+        public static int getNum()
+        {
+            return num;
+        }
+        static void Main(string[] args)
+        {
+            AddSumDelegate obj = new AddSumDelegate(AddSum);
+            obj.Invoke(100, 50);
+            MulDelegate obj1 = new MulDelegate(Mul);
+            obj1.Invoke( 50);
+            Console.ReadLine();
+        }
+        
+    }
+}
