8360b437d066cb2a0923ec23981751f0b6be75e9
manasachervirala <72153500+manasachervirala@users.noreply.github.com>
2020-10-09 15:10:14 +0530

    Add files via upload

---
+++ threads/multithreading.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace thm
+{
+    class Program
+    {
+        string Thread1()
+        {
+            for(int i=1; i<5;i++)
+            {
+                Console.WriteLine("Thread1" + i);
+            }
+            return ("THread1 ended");
+        }
+        string Thread2()
+        {
+            for (int i = 1; i < 2; i++)
+            {
+                Console.WriteLine("Thread2" + i);
+            }
+            return ("THread2 ended");
+        }
+        string Thread3()
+        {
+            for (int i = 1; i < 5; i++)
+            {
+                Console.WriteLine("Thread3" + i);
+            }
+            return ("THread3 ended");
+        }
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            Thread t1 = new Thread(delegate ()
+            {
+                Console.WriteLine(obj.Thread1());
+            });
+            t1.Start();
+            Thread t2 = new Thread(delegate ()
+            {
+                Console.WriteLine(obj.Thread2());
+            });
+            t2.Start();
+            Thread t3 = new Thread(delegate ()
+            {
+                Console.WriteLine(obj.Thread3());
+            });
+            t3.Start();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ threads/single threading.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ths
+{
+    class Program
+    {
+        string Thread1()
+        {
+            for(int i=1;i<=5;i++)
+            {
+                Console.WriteLine("Thread1 at position" + i);
+            }
+            return ("end of the thread1");
+        }
+        string Thread2()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("Thread2 at position" + i);
+            }
+            return ("end of the thread2");
+        }
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            Console.WriteLine(obj.Thread1());
+            Console.WriteLine(obj.Thread2());
+            Console.ReadKey();
+        }
+    }
+}
