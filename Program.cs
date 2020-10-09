b0ac5ed74bae9c59d5d3a3f70752853b78015f8e
manasachervirala <72153500+manasachervirala@users.noreply.github.com>
2020-10-09 14:47:40 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -3,54 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
-namespace stack
+namespace singlethreadexample
 {
+
+    class mythreads
+    {
+        public double sumThread1(int num)
+        {
+            int rem;
+            double sum = 0;
+            while (num != 0)
+            {
+                rem = num % 10;
+                sum = sum + rem;
+                num = num / 10;
+            }
+
+            return sum;
+        }
+
+        public string cancatthread2(string s1, string s2)
+        {
+            return s1 + s2;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            //created an object of stack
-            Stack<int> MyStack = new Stack<int>();
-            MyStack.Push(2);//push method takes one parameters/arguments
-            MyStack.Push(4);
-            MyStack.Push(6);
-            MyStack.Push(8);
-            MyStack.Push(10);
-            MyStack.Push(12);
-            MyStack.Push(14);
-           
-            Console.WriteLine("the top element {0}",MyStack.Peek());
-            //Console.ReadLine();
-            foreach(int stack in MyStack)
-            {
-                Console.WriteLine("The elements of my stack are is {0}" + stack);
-                Console.ReadLine();
-            }
+            Console.WriteLine("Main Thread Started");
+            mythreads obj = new mythreads();
+            double result = 0;
 
-            MyStack.Pop();
-            MyStack.Pop();
-            MyStack.Pop();
-            MyStack.Pop();
-            MyStack.Pop();
-            Console.WriteLine("after 4");
-            Console.WriteLine("peek {0}" ,MyStack.Peek());
-            foreach(int z in MyStack)
-                {
-                Console.WriteLine("stack elements {0}", z);
-            }
-           MyStack .Pop();
-            MyStack.Pop();
-            foreach(int z in MyStack)
-            {
-                Console.WriteLine("stack elements {0}", z);
-            }
+            Thread T1 = new Thread(() => { result = obj.sumThread1(78675); });
+            T1.Start();
+            T1.Join();
+            Console.WriteLine(result);
+            Console.WriteLine("thread 1 execution completed");
 
+            string res = null;
+            Thread T2 = new Thread(() => { res = obj.cancatthread2("Manasa", "CHervirala"); });
+
+            T2.Start();
+            T2.Join();
+            Console.WriteLine(res);
+            Console.WriteLine("thread 2 execution completed");
+
+            Console.WriteLine();
+            Console.WriteLine("Main Thread completed");
             Console.ReadKey();
-            //Console.WriteLine("peek element of the stack is:" + MyStack.Peek());// peek is to find the top most elements on the stack
-            //Console.WriteLine("poping the elements of the stack is:" + MyStack.Pop());// pop is to delete the elements in the stack
-            //Console.WriteLine("delete the elements in the pop operations" + MyStack.Pop());//
+
+
         }
-        
     }
 }
