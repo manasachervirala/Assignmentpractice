8d694cc1199c71127999d9844e3162af926b9ad7
manasachervirala <72153500+manasachervirala@users.noreply.github.com>
2020-11-10 10:03:55 +0530

    Add files via upload

---
+++ FirstMvc/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstMvcApplication.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult About()
+		{
+			ViewBag.Message = "My name is manasa.";
+			ViewBag.manasa = "congrats";
+
+			return View();
+		}
+
+		public ActionResult Contact()
+		{
+			ViewBag.Message = "Your manasa.chervirala.";
+
+			return View();
+		}
+	}
+}
---
+++ FirstMvc/MusicStroreController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstMvcApplication.Controllers
+{
+    public class MusicStroreController : Controller
+    {
+        // GET: MusicStrore
+		[ActionName("manasa")]
+		[HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+		[ActionName("karthik")]
+		[HttpPost]
+		public string Welcome()
+		{
+			return "hello this is welcome action message";
+			
+		}
+		[ActionName("Jacksaprrow")]
+		[HttpPatch]
+		public string ShowMusic(string MusicTitle)
+
+		{
+			return "you have selected" + MusicTitle + "Music";
+		}
+		public void function(string name)
+		{
+			Response.Write("You have selected" + "name" + "music");
+		}
+	
+    }
+}
